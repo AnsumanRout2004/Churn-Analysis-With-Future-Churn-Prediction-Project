d6313a77a0dd655dada17f925aeda977a85e4074
AnsumanRout2004 <160586912+AnsumanRout2004@users.noreply.github.com>
2025-10-26 11:05:34 +0530

    sql

--- Project Code for SQL Server Management Studion(SQL Queries).sql
+++ Project Code for SQL Server Management Studion(SQL Queries).sql
@@ -1,7 +1,1 @@
 
-Create View vw_ChurnData as
-	select * from prod_Churn where Customer_Status In ('Churned', 'Stayed')
-
-
-Create View vw_JoinData as
-	select * from prod_Churn where Customer_Status = 'Joined'
